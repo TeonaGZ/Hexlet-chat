41d01adc25dbd469b85be2e4c22ed63551bc4598
Teona Gatsereliya <teonazg@gmail.com>
2023-12-22 18:25:50 +0300

    Added rollbar

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import { io } from 'socket.io-client';
 import store from './slices/store.js';
 import resources from './locales/index.js';
@@ -13,6 +14,17 @@ const init = async () => {
   const socket = io();
   const i18n = i18next.createInstance();
 
+  const rollbarConfig = {
+    accessToken: 'bf21624cf472494e8cee3e0c63c9f2d9',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    environment: 'testenv',
+  };
+  function TestError() {
+    const a = null;
+    return a.hello();
+  }
+
   await i18n.use(initReactI18next).init({ resources, lng: 'ru', fallbackLng: 'ru' });
 
   return (
@@ -20,7 +32,12 @@ const init = async () => {
       <SocketProvider socket={socket}>
         <AuthProvider>
           <I18nextProvider i18n={i18n}>
-            <App />
+            <RollbarProvider config={rollbarConfig}>
+              <ErrorBoundary>
+                <App />
+                <TestError />
+              </ErrorBoundary>
+            </RollbarProvider>
           </I18nextProvider>
         </AuthProvider>
       </SocketProvider>
